89aae928dac8060276f761edf1153a9b7872b57c
Harri Evans <hevans9000@gmail.com>
2019-11-08 17:09:59 +0000

    feat: app header/nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,13 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
-const materialModules = [MatIconModule, MatToolbarModule];
+const materialModules = [MatIconModule, MatToolbarModule, MatButtonModule];
 const components = [AppComponent, FooterComponent, WelcomeComponent];
 
 @NgModule({
-  declarations: [...components],
+  declarations: [...components, HeaderComponent],
   imports: [
     ...materialModules,
     BrowserModule,
